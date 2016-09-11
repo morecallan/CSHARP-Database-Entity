f40f17ca1e5c5b78edf9d644eca7bea663ce7991
Callan Morrison <callan.l.morrison@gmail.com>
2016-09-11 08:57:15 -0500

    added base classes and enums

---
+++ NinjaDomain.Classes/src/NinjaDomain.Classes/Classes/Ninja.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NinjaDomain.Classes
+{
+    public class Ninja
+    {
+        public int NinjaId { get; set; }
+        public string Name { get; set; }
+        public bool ServedInOnibawan { get; set; }
+        public Clan Clan { get; set; }
+        public int ClanId { get; set; }
+        public List<NinjaEquipment> EquipmentOwned { get; set; }
+    }
+}
